a3abe08e7e70da6f3b689100b19e27c2cda999ef
moses <mosesfemi10@gmail.com>
2025-09-28 11:32:48 +0100

    add arithmetic_operation and main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#arithemetic operation function
+def perform_operation (num1, num2, operation):
+    if operation == "add":
+        result = num1 + num2
+        return result
+    elif operation == "subtract":
+        result = num1 - num2
+        return result
+    elif operation == "multiply":
+        result = num1 * num2
+        return result
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        result = num1 / num2
+        return result
+    else:
+        return "Error: Invalid operation"
